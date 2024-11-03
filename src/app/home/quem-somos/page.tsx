4a2d40c0cbf636581b7ccf8753fa959b483c453c
Vinicius Saes <viniciusaesouza@gmail.com>
2024-11-03 20:25:08 -0300

    fixed broken libs

--- src/app/home/quem-somos/page.tsx
+++ src/app/home/quem-somos/page.tsx
@@ -10,17 +10,17 @@ export default function QuemSomos() {
   return (
 	<div>
 		<CabecalhoLogin/>
-		<div className='h-[100vh]'>
-			<div className='flex  justify-around bg-gray-400/10 max-w-fit gap-x-80 items-center mx-auto px-4 py-8 rounded-lg shadow-lg shadow-black/60'>
+		<div className='h-[100vh] font-abeezee text-2xl mt-14'>
+			<div className='flex  justify-between bg-gray-400/10 max-w-fit gap-x-60 items-center mx-auto px-14 py-8 rounded-lg shadow-lg shadow-black/60'>
 				<div className=''>
-					<Image src={dodock} alt='Imagem do João Victor'/>
+					<Image className='mb-10' src={dodock} alt='Imagem do João Victor'/>
 					<p className=''>Nome: João Victor Michaeli</p>
 					<p className=''>Rm: 555678</p>
 					<p>Turma: 1TDSPK</p>
 					<p>GitHub: <a href="https://github.com/JoaoMichaeli">JoaoMichaeli</a></p>
 				</div>
 				<div className=''>
-					<Image src={saes} alt='Imagem do Vinícius Saes'/>
+					<Image className='mb-10' src={saes} alt='Imagem do Vinícius Saes'/>
 					<p className=''>Nome: Vinícius Saes de Souza</p>
 					<p>Rm: 554456</p>
 					<p>Turma: 1TDSPK</p>
